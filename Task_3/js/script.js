77e08d8601ea7f144f54be24ed3316c80579874a
SterrenHeer <sterrenheer@gmail.com>
2024-02-26 15:28:46 +0300

    add change of tabs on click and reformate some styles

--- Task_3/js/script.js
+++ Task_3/js/script.js
@@ -1,10 +1,12 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function() {  
 	let tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent')
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
 	function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = "none";
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
@@ -12,11 +14,24 @@ window.addEventListener('DOMContentLoaded', function() {
         });
 	}
 
-	function showTabContent(i) {
-        tabsContent[i].style.display = "block";
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
     
     hideTabContent();
-    showTabContent(2);
+    showTabContent();
+
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+	});
 });
